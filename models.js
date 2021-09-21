d863dec1b3f53c7cc71c73ecd4bfacca5968cbed
Amanda1024 <88503704+Amanda1024@users.noreply.github.com>
2021-09-21 12:37:49 -0700

    created models.js

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: { type: String, requires: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true },
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie ' }]
+});
+
+//Creates models (will auto-populate in the db as lowercase and plural)
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+//Allows index.js to import models
+module.exports.Movie = Movie;
+module.exports.User = User;
