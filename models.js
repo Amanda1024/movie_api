7a3edd2a7539a5cd4464cd6a5a65d0da7c434739
Amanda1024 <88503704+Amanda1024@users.noreply.github.com>
2021-09-22 15:57:22 -0700

    troubleshoot models

--- models.js
+++ models.js
@@ -27,10 +27,29 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie ' }]
 });
 
+// let genreSchema = mongoose.Schema({
+//     Name: { type: String, required: true },
+//     Description: { type: String, requires: true }
+// });
+
+// let directorSchema = mongoose.Schema({
+//     Name: { type: String, required: true },
+//     Bio: { type: String, required: true },
+//     Birth: { type: String, required: true },
+//     Death: { type: String },
+//     Movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+// });
+
+
+
 //Creates models (will auto-populate in the db as lowercase and plural)
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+// let Genre = mongoose.model('Genre', genreSchema);
+// let Director = mongoose.model('Director', directorSchema);
 
 //Allows index.js to import models
 module.exports.Movie = Movie;
 module.exports.User = User;
+// module.exports.Genre = Genre;
+// module.exports.Director = Director;
